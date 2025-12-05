ad35313ebadae5e834be15e68e5f10f515954178
Jordy van Eijk <jordy.vaneijk@vattenfall.com>
2025-12-05 11:45:39 +0100

    Refactor solution to read input from 'input.txt' and optimize digit concatenation logic

--- AdventOfCode2025/Day3/Solution.cs
+++ AdventOfCode2025/Day3/Solution.cs
@@ -7,7 +7,7 @@ public class Solution : ISolution<long>
     private long _total = 0;
     public async Task<long> Solve()
     {
-        var input = await InputHelper.GetInputAsync("testcase.txt");
+        var input = await InputHelper.GetInputAsync("input.txt");
         foreach (var line in input)
         {
             FindLargestTwoDigitsConcatInLine(line);
@@ -17,44 +17,26 @@ public class Solution : ISolution<long>
 
     private void FindLargestTwoDigitsConcatInLine(string line)
     {
+        int maxJoltage = int.MinValue;
         var digits = new List<int>();
         foreach (char c in line)
         {
             if (char.IsDigit(c))
                 digits.Add(c - '0');
         }
-        if (digits.Count >= 2)
+        for (int i = 0; i < digits.Count - 1; i++)
         {
-            // Find the index of the first largest digit
-            int firstIdx = 0;
-            for (int i = 1; i < digits.Count; i++)
+            for (int j = i + 1; j < digits.Count; j++)
             {
-                if (digits[i] > digits[firstIdx])
-                    firstIdx = i;
+                int joltage = digits[i] * 10 + digits[j];
+                if (joltage > maxJoltage)
+                    maxJoltage = joltage;
             }
-            // Find the index of the next largest digit, skipping firstIdx
-            int secondIdx = -1;
-            int secondMax = int.MinValue;
-            for (int i = 0; i < digits.Count; i++)
-            {
-                if (i == firstIdx) continue;
-                if (digits[i] > secondMax)
-                {
-                    secondMax = digits[i];
-                    secondIdx = i;
-                }
-            }
-            // Concatenate in their order of appearance
-            string concat = firstIdx < secondIdx
-                ? $"{digits[firstIdx]}{digits[secondIdx]}"
-                : $"{digits[secondIdx]}{digits[firstIdx]}";
-            int result = int.Parse(concat);
-            Console.WriteLine(concat);
-            _total += result;
         }
-        else if (digits.Count == 1)
+        if (maxJoltage != int.MinValue)
         {
-            _total += digits[0];
+            Console.WriteLine(maxJoltage);
+            _total += maxJoltage;
         }
     }
 }
